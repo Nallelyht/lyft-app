fb739ac0d6a23902595d407967733a01b40a41f0
Nallelyht <you@example.com>
2017-06-12 21:08:21 -0500

    mapa

---
+++ js/main-map.js
@@ -0,0 +1,33 @@
+var cargarPagina = function () {
+  obtenerUbicacion();
+};
+var obtenerUbicacion = function (e) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicion);
+  } else {
+    alert("Actualice su navegador");
+  }
+};
+
+
+var mostrarPosicion = function (posicion) {
+
+
+  var coordenadas = {
+    lat: posicion.coords.latitude, 
+    lng: posicion.coords.longitude
+  };
+  mostrarMapa(coordenadas);
+};
+
+var mostrarMapa = function (coordenadas) {
+  var map = new google.maps.Map($('.map')[0], {
+    zoom: 16,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
+$(document).ready(cargarPagina)
